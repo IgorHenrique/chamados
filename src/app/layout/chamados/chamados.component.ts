3d2ad2203ab6ba9525c0770021e062da3dcd5ba0
Igor Henrique <igorhenrioli@gmail.com>
2019-06-12 00:07:24 -0300

    lendo xml

--- src/app/layout/chamados/chamados.component.ts
+++ src/app/layout/chamados/chamados.component.ts
@@ -4,6 +4,7 @@ import { Servico } from 'src/app/model/servico';
 import { ServicosService } from 'src/app/shared/services/servicos.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-chamados',
@@ -14,6 +15,8 @@ export class ChamadosComponent implements OnInit {
   public isLoading: boolean;
   servicos : Servico[];
   formGroup : FormGroup;
+  titulo: string;
+  observacao: string;
   constructor(public servicoService : ServicosService,
     public chamadoService : ChamadosService,
     public formBuilder : FormBuilder,
@@ -46,5 +49,25 @@ export class ChamadosComponent implements OnInit {
       })
     }
   }
+
+  lerArquivo(event) {
+    if (event.target.files && event.target.files[0]) {
+      const arquivo = event.target.files[0];
+
+      var reader = new FileReader();
+      reader.onload = (event) => {
+        let parse = new DOMParser();
+        let arquivoXml = parse.parseFromString(event.target.result, "text/xml");
+        let problema = arquivoXml.getElementsByTagName("PROBLEMA")[0];
+        let observacao = arquivoXml.getElementsByTagName("OBSERVACAO")[0]; 
+        this.titulo = problema ? problema.innerHTML : "";
+        this.observacao = observacao ? observacao.innerHTML : "";
+  
+      }
+      reader.readAsText(arquivo);
+    }
+  }
+
+  teste() {}
 
 }
